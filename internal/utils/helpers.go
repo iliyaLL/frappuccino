dfed51efbf3e1b54f875014c8ced0a8676e33ef2
iliya <limilia248@gmail.com>
2025-03-17 22:50:06 +0500

    add gitignore

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// sending responses in the json format
+//
+//	{
+//		"error": "Internal Server Error"
+//	}
+type Response map[string]interface{}
+
+func SendJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
